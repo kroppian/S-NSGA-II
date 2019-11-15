495d71db7f47b0e588acaf4b3bd960f2fbfcee9d
Ian Kropp <iankropp@gmail.com>
2019-11-14 19:45:11 -0500

    First swing at fitness

--- CropoverTests.py
+++ CropoverTests.py
@@ -11,32 +11,41 @@ class CropoverTests(Problem):
 
         self.days = 120
         self.appCount = 16
-
+    
+        # All optima will be zeros, except...
         self.optima = np.zeros(self.days)
+
+        # The few days that will non-zero
         self.appDays = np.random.randint(0,self.days-1,self.appCount)
 
+        # Fill in those days
         self.optima[self.appDays] = np.random.randint(self.fit_min, self.fit_max ,len(self.appDays))
 
+        # Limits
         self.fit_l = np.zeros(self.days)
-        
         self.fit_u = np.ones(self.days)*self.fit_max
 
         super().__init__(n_var=self.days,
                          n_obj=2,
                          n_constr=0,
                          xl=self.fit_l,
-                         xu=self.fit_u
-        
-         
-            
+                         xu=self.fit_u)
 
-
+    def foo(bar):
+        return bar
 
     def _evaluate(self, x, out, *args, **kwargs):
-        self.fit_min = 1
+   
+        print(len(x)) 
+        f1 = np.sum(x)
+        
+        # Treat the 
+        f_ind = np.power(x + self.optima, 2)
+
+        f2 = np.sum(f_ind)
 
         out["F"] = anp.column_stack([f1, f2])
-        out["G"] = anp.column_stack([g1, g2])
+
 
 
     # --------------------------------------------------
@@ -63,6 +72,6 @@ class CropoverTests(Problem):
 #        a, b = np.column_stack([x1_a, x2]), np.column_stack([x1_b, x2])
 #        return stack(a,b, flatten=flatten)
 
-#problem = MyProblem()
+crossovertests = CropoverTests()
 
 
