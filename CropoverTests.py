d4ecb3089c78478ab7798923b15e758bd89b8b6e
Ian Kropp <iankropp@gmail.com>
2019-11-18 15:24:17 -0500

    Fixing the objective function to make the objectives conflict

--- CropoverTests.py
+++ CropoverTests.py
@@ -49,7 +49,7 @@ class CropoverTests(Problem):
   
         f1 = np.sum(x, axis=1)
         
-        f_ind = np.power(x + self.optima, 2) + self.heights
+        f_ind = -np.power(x - self.optima, 2) + self.heights
 
         f2 = np.sum(f_ind, axis=1)
 
@@ -58,30 +58,6 @@ class CropoverTests(Problem):
     def get_optimal(self):
         return np.sum(self.heights)
 
-
-    # --------------------------------------------------
-    # Pareto-front - not necessary but used for plotting
-    # --------------------------------------------------
-#   def _calc_pareto_front(self, flatten=True, **kwargs):
-#       f1_a = np.linspace(0.1**2, 0.4**2, 100)
-#       f2_a = (np.sqrt(f1_a) - 1)**2
-#
-#       f1_b = np.linspace(0.6**2, 0.9**2, 100)
-#       f2_b = (np.sqrt(f1_b) - 1)**2
-#
-#       a, b = np.column_stack([f1_a, f2_a]), np.column_stack([f1_b, f2_b])
-#       return stack(a, b, flatten=flatten)
-
-    # --------------------------------------------------
-    # Pareto-set - not necessary but used for plotting
-    # --------------------------------------------------
-#    def _calc_pareto_set(self, flatten=True, **kwargs):
-#        x1_a = np.linspace(0.1, 0.4, 50)
-#        x1_b = np.linspace(0.6, 0.9, 50)
-#        x2 = np.zeros(50)
-#
-#        a, b = np.column_stack([x1_a, x2]), np.column_stack([x1_b, x2])
-#        return stack(a,b, flatten=flatten)
 
 crossovertests = CropoverTests()
 
