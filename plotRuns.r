ddff740eaa1ae72dedebc03bf413b591e1bbe954
Ian Kropp <iankropp@gmail.com>
2019-11-19 16:59:31 -0500

    Have side by side plotting finished (and good results!!)

--- plotRuns.r
+++ plotRuns.r
@@ -2,14 +2,29 @@
 
 args = commandArgs(trailingOnly=TRUE)
 
+withFiles = args[grepl("with_run.*\\.csv", args)]
+withoutFiles = args[grepl("without_run.*\\.csv", args)]
+#x11()
+
+
 i = 0
-for (file in args){
+for (file in withFiles){
+  print(file)
   data = read.csv(file)
   if (i == 0)
-    plot(data[,1],data[,2],col = rainbow(length(args))[i])
+    plot(data[,1],data[,2],col = "red")
   else
-    points(data[,1],data[,2],col = rainbow(length(args))[i])
-  
+    points(data[,1],data[,2],col = "red")
+  i = i + 1
+}
+
+for (file in withoutFiles){
+  print(file)
+  data = read.csv(file)
+  if (i == 0)
+    points(data[,1],data[,2],col = "blue")
+  else
+    points(data[,1],data[,2],col = "blue")
   i = i + 1
 }
 
