fb3eb0398c0ab274bc88fe91b085d3394ab7c4d1
Ian Kropp <iankropp@gmail.com>
2020-09-22 16:25:48 -0400

    Have a sample customized NSGA-II set up

---
+++ sparseEATests/mymain.m
@@ -0,0 +1,17 @@
+
+
+% Path to the install path of plat EMO 
+platEMOPath = '/Users/iankropp/Projects/platEMO/';
+workingDir = pwd;
+addpath(platEMOPath);
+addpath(workingDir);
+addpath(strcat(workingDir, '/NSGAII_SS'));
+
+
+% 
+fprintf("%s\n", currentDir);
+
+main('-algorithm',@NSGAII_SS,'-problem',@DTLZ2,'-N',200,'-M',10);
+
+
+cd(workingDir);
