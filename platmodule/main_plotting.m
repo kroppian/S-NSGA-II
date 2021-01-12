9c67b7a38d6ee1aa5dbafbd0099d4f8c22936b00
Ian Kropp <iankropp@gmail.com>
2021-01-12 11:00:56 -0500

    Added time plotting

--- platmodule/main_plotting.m
+++ platmodule/main_plotting.m
@@ -10,54 +10,73 @@ numAlgorithms = size(HVResults, 3);
 algorithmColors = {'black', 'red', 'blue'};
 
 
+
 % results
 % dim. two: the number of decision variables
 % dim. one: algorithm
-means = ones(numDecisionVars, numAlgorithms);
-upperInts = ones(numDecisionVars, numAlgorithms);
-lowerInts = ones(numDecisionVars, numAlgorithms);
+HVMeans = ones(numDecisionVars, numAlgorithms);
+HVupperInts = ones(numDecisionVars, numAlgorithms);
+HVlowerInts = ones(numDecisionVars, numAlgorithms);
+
+timeMeans = ones(numDecisionVars, numAlgorithms);
+timeUpperInts = ones(numDecisionVars, numAlgorithms);
+timelowerInts = ones(numDecisionVars, numAlgorithms);
 
 
 % For every algorithm
 for alg = 1:numAlgorithms
     
-    
     algHV = HVResults(:,:,alg);
-    % For every number of decision variables
     
+    algTimes = timeResults(:,:,alg);
+    
+    % For every number of decision variables
+
     for decVar = 1:numDecisionVars
-        
+        %% HV processing
+
         hyperVols = algHV(:,decVar);
         
         % Calculate mean
         meanHV = mean(hyperVols);
         
-        % Calculate standard deviation
-        stdDevHV = std(hyperVols);
+        HVMeans(decVar, alg) = meanHV;
         
-        means(decVar, alg) = meanHV;
-        
-        % Lazy CIs
+        % CIs
         stdErr = std(hyperVols)/sqrt(length(hyperVols));
         ts = tinv([0.025  0.975],length(hyperVols)-1);
         CI  = meanHV + ts*stdErr;
-         
         
-        upperInts(decVar, alg) = max(CI);
-        lowerInts(decVar, alg) = min(CI);
+        HVupperInts(decVar, alg) = max(CI);
+        HVlowerInts(decVar, alg) = min(CI);
          
+        %% time processing
+        times = algTimes(:,decVar);
+
+        % Calculate mean
+        meanTime = mean(times);
+        
+        timeMeans(decVar, alg) = meanTime;
+        
+        % CIs
+        stdErr = std(times)/sqrt(length(times));
+        ts = tinv([0.025  0.975],length(times)-1);
+        CI  = meanTime + ts*stdErr;
+        
+        timeUpperInts(decVar, alg) = max(CI);
+        timelowerInts(decVar, alg) = min(CI);
+        
     end
 
 end
 
-% Plot means
-
+%% Plot HV results
 for alg = 1:numAlgorithms
     
     color = algorithmColors{alg};
     
-    lowerInterval = lowerInts(:,alg);
-    upperInterval = upperInts(:,alg);
+    lowerInterval = HVlowerInts(:,alg);
+    upperInterval = HVupperInts(:,alg);
     
     x = [1:numDecisionVars, fliplr(1:numDecisionVars)];
     y = [lowerInterval; flipud(upperInterval)]';
@@ -69,9 +88,42 @@ for alg = 1:numAlgorithms
     hold on
 
     
-    plot(means(:,alg), color);
+    plot(HVMeans(:,alg), color);
     
 end
 
+legend("SparseEA 95% conf. int", "SparseEA mean",  "NSGA-II with SPS 95% conf. int", "NSGA-II with SPS",  "NSGA-II 95% conf. int", "NSGA-II")
+
+figure
+
+%% Plot time results
+
+%timeMeans = ones(numDecisionVars, numAlgorithms);
+%timeUpperInts = ones(numDecisionVars, numAlgorithms);
+%timelowerInts = ones(numDecisionVars, numAlgorithms);
+
+
+for alg = 1:numAlgorithms
+    
+    color = algorithmColors{alg};
+    
+    lowerInterval = timeUpperInts(:,alg);
+    upperInterval = timelowerInts(:,alg);
+    
+    x = [1:numDecisionVars, fliplr(1:numDecisionVars)];
+    y = [lowerInterval; flipud(upperInterval)]';
+    fill(x,y, 1,....
+        'facecolor',color, ...
+        'edgecolor','none', ...
+        'facealpha', 0.3);
+   
+    hold on
+
+    
+    plot(timeMeans(:,alg), color);
+    
+end
+
+legend("SparseEA 95% conf. int", "SparseEA mean",  "NSGA-II with SPS 95% conf. int", "NSGA-II with SPS",  "NSGA-II 95% conf. int", "NSGA-II")
 
 
