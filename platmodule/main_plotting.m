324d559555ed81481d99ce46df189488cb76e617
Ian Kropp <iankropp@gmail.com>
2021-02-01 11:19:58 -0500

    Adapting this script to handle sparsity runs

--- platmodule/main_plotting.m
+++ platmodule/main_plotting.m
@@ -1,65 +1,88 @@
 
 clear
 
-%% Comparative runs parameters (uncomment/comment to use/not use)
-% Pull just one of these result files to get a bit of metadata on the run
-% load('data/runResultsSMOP8.mat');
-% 
-% % Generate the number of colors needed automatically 
-% alg_count = 3;
-% colors = turbo;
-% color_count = size(colors, 1);
-% color_step_size = floor(color_count/alg_count);
-% color_indices = (1:alg_count)*color_step_size;
-% algorithmColors = colors(color_indices,:);
-% 
-% legend_entries = {'SparseEA 95% conf. int', 'SparseEA mean',  ...
-%     'NSGA-II with SPS 95% conf. int', 'NSGA-II with SPS',  ...
-%     'NSGA-II 95% conf. int', 'NSGA-II'};
+%% Global controls 
 
-%% Effective runs parameters (uncomment/comment to use/not use)
-% Pull just one of these result files to get a bit of metadata on the run
-load('data/runResults_comparative_SMOP1.mat');
+% true to make the dependent variable # of decision variables
+% false to make the dependent variable sparsity % 
+indep_var_dec_vars = false;
 
-% Generate the number of colors needed automatically 
-alg_count = 6;
-colors = turbo;
-color_count = size(colors, 1);
-color_step_size = floor(color_count/alg_count);
-color_indices = (1:alg_count)*color_step_size;
-algorithmColors = colors(color_indices,:);
+% true to plot for comparative runs
+% false to plot effective runs
+comparative_runs = true;
 
-legend_entries = {'MOPSO 95% conf. int', 'MOPSO mean',  ...
-    'MOPSO with SPS 95% conf. int', 'MOPSO with SPS mean',  ...
-    'MOEADDE 95% conf. int', 'MOEADDE mean', ...
-    'MOEADDE with SPS 95% conf. int', 'MOEADDE with SPS mean', ...
-    'NSGA-II 95% conf. int', 'NSGA-II mean', ...
-    'NSGA-II with SPS 95% conf. int', 'NSGA-II with SPS mean'};
+%% Comparative versus effective options
+if comparative_runs
+
+    %% Comparative runs parameters (uncomment/comment to use/not use)
+    % Pull just one of these result files to get a bit of metadata on the run
+    load('data/runResults_comparative_sparsity_SMOP1.mat');
+
+    % Generate the number of colors needed automatically 
+
+    algorithmColors = [255, 25, 25; 0, 0, 230];
+
+    algorithmColors = algorithmColors/255;
+
+    legend_entries = {'SparseEA 95% conf. int', 'SparseEA mean',  ...
+        'NSGA-II with SPS 95% conf. int', 'NSGA-II with SPS'};
+
+else
+    %% Effective runs parameters (uncomment/comment to use/not use)
+    % Pull which test cases to run
+    load('data/runResults_comparative_SMOP8.mat');
+    
+    % Generate the number of colors needed automatically 
+    alg_count = 6;
+    colors = hsv;
+    color_count = size(colors, 1);
+    color_step_size = floor(color_count/alg_count);
+    color_indices = (1:alg_count)*color_step_size;
+    algorithmColors = colors(color_indices,:);
+    
+    legend_entries = {'MOPSO 95% conf. int', 'MOPSO mean',  ...
+        'MOPSO with SPS 95% conf. int', 'MOPSO with SPS mean',  ...
+        'MOEADDE 95% conf. int', 'MOEADDE mean', ...
+        'MOEADDE with SPS 95% conf. int', 'MOEADDE with SPS mean', ...
+        'NSGA-II 95% conf. int', 'NSGA-II mean', ...
+        'NSGA-II with SPS 95% conf. int', 'NSGA-II with SPS mean'};
+
+end
+
+%% # decision vars vs % sparsity options
+Dz = [100, 500, 1000, 2500, 5000, 7500];
+sparsities = (1-linspace(0.05, 0.45,9))*100;
+
+if indep_var_dec_vars
+    dependentVars = Dz;
+    x_label = 'Decision variables';
+else
+    dependentVars = sparsities;
+    x_label = 'Sparsity (%)';
+end
 
 
 %% Remaining parameters 
+
 
 metricLabels = {'HV vs # of decision variables', 'Runtime vs # of decision variables', 'Number of non-dominated solutions vs # of decision variables'};
 yLabels = {'HV', 'Runtime (seconds)', 'Number of non-dominated solutions'};
 results = {HVResults, timeResults, noNonDoms};
 
 numRepetitions = size(HVResults, 1);
-numDecisionVars = size(HVResults, 2);
+numDependentVars = size(HVResults, 2);
 numAlgorithms = size(HVResults, 3);
-
-
-Dz = [100, 500, 1000, 2500, 5000, 7500];
-
 
 % results
 % dim. two: the number of decision variables
 % dim. one: algorithm
-template = ones(numDecisionVars, numAlgorithms);
+template = ones(numDependentVars, numAlgorithms);
 
 globalMeans = {template, template};
 globalUpperInts = {template, template};
 globalLowerInts = {template, template};
 
+%% Calc results
 
 % For every metric
 for m = 1:numel(results)
@@ -75,7 +98,7 @@ for m = 1:numel(results)
 
         % For every number of decision variables
 
-        for decVar = 1:numDecisionVars
+        for decVar = 1:numDependentVars
             
             %% HV processing
 
@@ -113,7 +136,7 @@ for m = 1:numel(results)
         lowerInterval = globalLowerInts{m}(:,alg);
         upperInterval = globalUpperInts{m}(:,alg);
 
-        x = [Dz, fliplr(Dz)];
+        x = [dependentVars, fliplr(dependentVars)];
         y = [lowerInterval; flipud(upperInterval)]';
         fill(x,y, 1,....
             'facecolor',color, ...
@@ -122,11 +145,11 @@ for m = 1:numel(results)
 
         hold on
         
-        plot(Dz,globalMeans{m}(:,alg), 'Color', color);
+        plot(dependentVars,globalMeans{m}(:,alg), 'Color', color);
         
         title(metricLabels{m});
 
-        xlabel('Decision variables');
+        xlabel(x_label);
         ylabel(yLabels{m});
         
     end
