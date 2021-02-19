23aee7190221a39fd41786e1692139a58fd7930f
Ian Kropp <iankropp@gmail.com>
2021-02-19 15:20:45 -0500

    Add functionality for sparsity variation

--- platmodule/main_tables.m
+++ platmodule/main_tables.m
@@ -1,14 +1,43 @@
 clear;
 
-%% Comparative runs parameters (uncomment/comment to use/not use)
+%% Comparative runs parameters for # of decision variables 
+%  (uncomment/comment to use/not use)
+% algorithmsUsed = {'SparseEA', 'NSGAII-SPS'};
+% load('data/runResults_comparative_decVar_SMOP1.mat');
+% decVarsUsed = [100, 500, 1000, 2500, 5000, 7500];
+% 
+% 
+% fileNames = {'data/runResults_comparative_decVar_SMOP1.mat', ... 
+%              'data/runResults_comparative_decVar_SMOP2.mat', ... 
+%              'data/runResults_comparative_decVar_SMOP3.mat', ...
+%              'data/runResults_comparative_decVar_SMOP4.mat', ...
+%              'data/runResults_comparative_decVar_SMOP5.mat', ...
+%              'data/runResults_comparative_decVar_SMOP6.mat', ...
+%              'data/runResults_comparative_decVar_SMOP7.mat', ...
+%              'data/runResults_comparative_decVar_SMOP8.mat'};
+% 
+% 
+% label = "comparative";
+% % Set to true if the run is on decision variables. False for sparsrity 
+% usesDecVar = true
+
+%% Comparative runs parameters for # of decision variables 
 algorithmsUsed = {'SparseEA', 'NSGAII-SPS'};
-load('data/runResultsSMOP1.mat');
-decVarsUsed = decVars;
-fileNames = {'data/runResultsSMOP1.mat', 'data/runResultsSMOP2.mat', ...
- 'data/runResultsSMOP3.mat', 'data/runResultsSMOP4.mat', 'data/runResultsSMOP5.mat', ...
- 'data/runResultsSMOP6.mat', 'data/runResultsSMOP7.mat', 'data/runResultsSMOP8.mat'};
+load('data/runResults_comparative_sparsity_SMOP1.mat');
+sparsitiesUsed = (1-linspace(0.05, 0.45,9))*100;
+
+fileNames = {'data/runResults_comparative_sparsity_SMOP1.mat', ... 
+             'data/runResults_comparative_sparsity_SMOP2.mat', ... 
+             'data/runResults_comparative_sparsity_SMOP3.mat', ...
+             'data/runResults_comparative_sparsity_SMOP4.mat', ...
+             'data/runResults_comparative_sparsity_SMOP5.mat', ...
+             'data/runResults_comparative_sparsity_SMOP6.mat', ...
+             'data/runResults_comparative_sparsity_SMOP7.mat', ...
+             'data/runResults_comparative_sparsity_SMOP8.mat'};
 
 label = "comparative";
+% Set to true if the run is on decision variables. False for sparsrity 
+usesDecVar = false;
 
 %% Effective run parameters (uncomment/comment to use/not use)
 % algorithmsUsed = {'MOPSO', 'MOPSO-SPS', 'MOEADDE', 'MOEADDE-SPS', 'NSGAII', 'NSGAII-SPS'};
@@ -25,14 +54,24 @@ label = "comparative";
 testProblemsUsed = {'SMOP1','SMOP2','SMOP3','SMOP4','SMOP5','SMOP6','SMOP7','SMOP8'};
 
 numRepetitions = size(HVResults,1);
-numPossibleDecVars = numel(decVarsUsed);
 numTestProbs = numel(fileNames);
 numAlgorithms = numel(algorithmsUsed);
+if usesDecVar
+    numDependentVar = numel(decVarsUsed);
+else
+    numDependentVar = numel(sparsitiesUsed);
+end
 
-numRows = numAlgorithms*numPossibleDecVars*numTestProbs*numRepetitions;
+numRows = numAlgorithms*numDependentVar*numTestProbs*numRepetitions;
+
 
 % Columns of the table
-numDecVars = ones(numRows,1)*-1;
+if usesDecVar
+    numDecVars = ones(numRows,1)*-1;
+else
+    sparsities = ones(numRows,1)*-1;
+end
+
 algorithm = cell(numRows,1);
 testProbs = cell(numRows,1);
 hv = ones(numRows,1)*-1;
@@ -52,7 +91,7 @@ for t = 1:numTestProbs
     for a = 1:numAlgorithms
         
         % for each number of decision variables
-        for d = 1:numPossibleDecVars
+        for d = 1:numDependentVar
             
             % For each repetition
             for r = 1:numRepetitions
@@ -62,11 +101,15 @@ for t = 1:numTestProbs
                 % Test problem
                 testProbs{row} = testProblemsUsed{t};
 
-                % Decision variables
-                decVar = decVarsUsed(d);
-
-                numDecVars(row) = decVar;
-
+                if usesDecVar
+                    % Decision variables
+                    decVar = decVarsUsed(d);
+                    numDecVars(row) = decVar;
+                else
+                    sparsity = sparsitiesUsed(d);
+                    sparsities(row) = sparsity;
+                end
+                
                 %% algorithms
                 currentAlgorithm = algorithmsUsed{a};
 
@@ -93,10 +136,18 @@ end
 
 %% Make the table
 
-resultsTable = table(numDecVars, algorithm, testProbs, repetition, hv, runTimes, numNonDom);
 
-filename = strcat(label, '_resultsTable.mat');
+if usesDecVar
+    resultsTable = table(numDecVars, algorithm, testProbs, repetition, hv, runTimes, numNonDom);
+    filename = strcat(label, '_decVar_resultsTable.mat');
+    save(filename, 'resultsTable', 'algorithmsUsed', 'testProblemsUsed', 'decVarsUsed');
+else
+    resultsTable = table(sparsities, algorithm, testProbs, repetition, hv, runTimes, numNonDom);
+    filename = strcat(label, '_sparsity_resultsTable.mat');
+    save(filename, 'resultsTable', 'algorithmsUsed', 'testProblemsUsed', 'sparsitiesUsed');
+end
 
-save(filename, 'resultsTable', 'algorithmsUsed', 'testProblemsUsed', 'decVarsUsed');
+
+
 
 
