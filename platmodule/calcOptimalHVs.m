180bee4fd78b4fadd1f997b758d1525d71d712b4
Ian Kropp <iankropp@gmail.com>
2021-03-09 13:43:24 -0500

    Fixing this script

--- platmodule/calcOptimalHVs.m
+++ platmodule/calcOptimalHVs.m
@@ -3,7 +3,6 @@ clear;
 
 refPoint = [7,7];
 
-
 % Path to the install path of plat EMO 
 addpath(genpath('/Users/iankropp/Projects/platEMO/'));
 addpath(genpath('/Users/iankropp/Projects/platEMO/Public'));
@@ -12,13 +11,17 @@ addpath(genpath('/Users/iankropp/Projects/platEMO/Public'));
 [workingDir, name, ext]= fileparts(mfilename('fullpath'));
 addpath(workingDir);
 
-fncs = {SMOP1, SMOP2, SMOP3, SMOP4, SMOP5, SMOP6, SMOP7, SMOP8};
+fncs = {@SMOP1, @SMOP2, @SMOP3, @SMOP4, @SMOP5, @SMOP6, @SMOP7, @SMOP8};
 fncs_names = {'SMOP1', 'SMOP2', 'SMOP3', 'SMOP4', 'SMOP5', 'SMOP6', 'SMOP7', 'SMOP8'};
 
-parfor f = 1:numel(fncs)
-    ref_front = fncs{f}.Global.PF;
+
+
+for f = 1:numel(fncs)
+    Global = GLOBAL_SPS('-algorithm',@NSGAII,'-evaluation', 20000,'-problem',fncs{f},'-N',100,'-M',2, '-D', 100,'-outputFcn', @nop);
+    
+    ref_front = Global.problem.PF(100);
+        
     hv = CalHV(ref_front, refPoint);
     fprintf("HV for %s is %f\n", fncs_names{f}, hv);
-
 end
 
