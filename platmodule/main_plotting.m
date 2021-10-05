fcf0f9aca2439cf0b59ee5c6fb49d1deb7eb8883
Ian Kropp <iankropp@gmail.com>
2021-10-05 14:12:50 -0400

    Ignore previous commit. Plotting all SMOP[0-8] plots at once

--- platmodule/main_plotting.m
+++ platmodule/main_plotting.m
@@ -14,14 +14,56 @@ indep_var_dec_vars = true;
 comparative_runs = true;
 
 % CHANGE ME 3
-input_file = 'Z:\Gilgamesh\kroppian\spsRuns\2021-10-04\runResults_comparative_decVar_SMOP1.mat';
-load(input_file);
+runs = {
+   '/Volumes/data/Gilgamesh/kroppian/spsRuns/2021-10-04/runResults_comparative_decVar_SMOP1.mat', ...  
+   '/Volumes/data/Gilgamesh/kroppian/spsRuns/2021-10-04/runResults_comparative_decVar_SMOP2.mat', ...
+   '/Volumes/data/Gilgamesh/kroppian/spsRuns/2021-10-04/runResults_comparative_decVar_SMOP3.mat', ...
+   '/Volumes/data/Gilgamesh/kroppian/spsRuns/2021-10-04/runResults_comparative_decVar_SMOP4.mat', ...
+   '/Volumes/data/Gilgamesh/kroppian/spsRuns/2021-10-04/runResults_comparative_decVar_SMOP5.mat', ...
+   '/Volumes/data/Gilgamesh/kroppian/spsRuns/2021-10-04/runResults_comparative_decVar_SMOP6.mat', ...
+   '/Volumes/data/Gilgamesh/kroppian/spsRuns/2021-10-04/runResults_comparative_decVar_SMOP7.mat', ...
+   '/Volumes/data/Gilgamesh/kroppian/spsRuns/2021-10-04/runResults_comparative_decVar_SMOP8.mat'
+
+   };
+
+% runs = {
+%    'data/runResults_effective_sparsity_SMOP1.mat', ...  
+%    'data/runResults_effective_sparsity_SMOP2.mat', ...
+%    'data/runResults_effective_sparsity_SMOP3.mat', ...
+%    'data/runResults_effective_sparsity_SMOP4.mat', ...
+%    'data/runResults_effective_sparsity_SMOP5.mat', ...
+%    'data/runResults_effective_sparsity_SMOP6.mat', ...
+%    'data/runResults_effective_sparsity_SMOP7.mat', ...
+%    'data/runResults_effective_sparsity_SMOP8.mat'};
+% runs = {
+%    'data/runResults_effective_decVar_SMOP1.mat', ...  
+%    'data/runResults_effective_decVar_SMOP2.mat', ...
+%    'data/runResults_effective_decVar_SMOP3.mat', ...
+%    'data/runResults_effective_decVar_SMOP4.mat', ...
+%    'data/runResults_effective_decVar_SMOP5.mat', ...
+%    'data/runResults_effective_decVar_SMOP6.mat', ...
+%    'data/runResults_effective_decVar_SMOP7.mat', ...
+%    'data/runResults_effective_decVar_SMOP8.mat'};
+
+
+
+% Load one as a template for the rest of the runs
+load(runs{1});
 
 % CHANGE ME 4
 save_to_file = false;
 
 % CHANGE ME 5
-hv_ref = 1;
+% HV = 1
+% run time = 2
+% NNDS = 3
+metric = 1;
+
+% CHANGE ME 6
+grid_layout = false;
+
+% CHANGE ME 7
+hv_ref = 2; 
 
 %% Comparative versus effective options
 if comparative_runs
@@ -72,35 +114,57 @@ end
 
 %% Remaining parameters 
 
-
 metricLabels = {'HV vs # of decision variables', 'Runtime vs # of decision variables', 'Number of non-dominated solutions vs # of decision variables'};
-abbrMetricLabels = {'hv', 'runtime', 'NNDS'};
-yLabels = {'HV', 'Runtime (seconds)', 'Number of non-dominated solutions'};
-results = {HVResults, timeResults, noNonDoms};
+abbrMetricLabels = {'hv', 'runtime', 'NDS'};
+yLabels = {'HV', 'Runtime log(seconds)', 'Number of non-dominated solutions'};
+testProbLabels = {'SMOP1', 'SMOP2', 'SMOP3', 'SMOP4', 'SMOP5', 'SMOP6', 'SMOP7', 'SMOP8'};
 
 numRepetitions = size(HVResults, 1);
 numDependentVars = size(HVResults, 2);
 numAlgorithms = size(HVResults, 3);
+numRuns = numel(runs);
 
-hv_ind = 1;
+%% Calculate optimal HV
+if metric == 1
+    optimal_hvs = ones(1,8);
 
-% results
-% dim. two: the number of decision variables
-% dim. one: algorithm
-template = ones(numDependentVars, numAlgorithms);
+    fncs = {@SMOP1, @SMOP2, @SMOP3, @SMOP4, @SMOP5, @SMOP6, @SMOP7, @SMOP8};
+    fncs_names = {'SMOP1', 'SMOP2', 'SMOP3', 'SMOP4', 'SMOP5', 'SMOP6', 'SMOP7', 'SMOP8'};
 
-globalMeans = {template, template};
-globalUpperInts = {template, template};
-globalLowerInts = {template, template};
 
-%% Calc results
+    for f = 1:numel(fncs)
+        Global = GLOBAL_SPS('-algorithm',@NSGAII,'-evaluation', 20000,'-problem',fncs{f},'-N',100,'-M',2, '-D', 100,'-outputFcn', @nop);
 
-% For every metric
-for m = 1:numel(results)
+        ref_front = Global.problem.PF(100);
+
+        hv = CalHV(ref_front, [hv_ref, hv_ref]);
+        optimal_hvs(f) = hv;
+    end
+end
+
+%% Plotting
+
+plotno = 1;
+
+for r = 1:numRuns
+            
+    input_file = runs{r};
+    load(input_file);
+    results = {HVResults, timeResults, noNonDoms};
     
-    label = metricLabels{m};
-    metricResults = results{m};
-    
+    % results
+    % dim. two: the number of decision variables
+    % dim. one: algorithm
+    template = ones(numDependentVars, numAlgorithms);
+
+    globalMeans = {template, template};
+    globalUpperInts = {template, template};
+    globalLowerInts = {template, template};
+
+    %% Calc result summaries (mean, confidence intervals)
+    label = metricLabels{metric};
+    metricResults = results{metric};
+
     % For every algorithm
     for alg = 1:numAlgorithms
 
@@ -109,12 +173,11 @@ for m = 1:numel(results)
         % For every number of decision variables
 
         for decVar = 1:numDependentVars
-            
             %% metric processing
 
-            decResults = algResults(:,decVar);
+            decResults = algResults(:,decVar); % r == 3 && alg == 2
 
-            if m == hv_ind
+            if metric == 1
                 % Pull the appropriate HV from the record
                 decResults = arrayfun(@(x)(x{1}(hv_ref)), decResults, 'UniformOutput', false);
                 decResults = arrayfun(@(x)(x{1}), decResults);
@@ -124,66 +187,124 @@ for m = 1:numel(results)
             % Calculate mean
             decMean = mean(decResults);
 
-            globalMeans{m}(decVar, alg) = decMean;
-
-            % CIs
-%             stdErr = std(decResults)/sqrt(length(decResults));
-%             ts = tinv([0.025  0.975],length(decResults)-1);
-%             CI  = decMean + ts*stdErr;
+            globalMeans{metric}(decVar, alg) = decMean;
+    
+            % Calculate confidence intervals
             CI = bootci(30, @mean, decResults);
 
-            globalUpperInts{m}(decVar, alg) = max(CI);
-            globalLowerInts{m}(decVar, alg) = min(CI);
+            globalUpperInts{metric}(decVar, alg) = max(CI);
+            globalLowerInts{metric}(decVar, alg) = min(CI);
 
         end
 
-    end
-end
-
-%% Plot  results
-
-% For every metric
-for m = 1:numel(results)
-    
-    figure
         
-    for alg = 1:numAlgorithms
+    end
 
+    %% Plot  results
+    if grid_layout
+        subplot(3,3,plotno);
+    else
+        figure
+    end
+
+    y_min = 5000000;
+    y_max = -5000000;
+    
+    for alg = 1:numAlgorithms
+            
         color = algorithmColors(alg,:);
 
-        lowerInterval = globalLowerInts{m}(:,alg);
-        upperInterval = globalUpperInts{m}(:,alg);
+        lowerInterval = globalLowerInts{metric}(:,alg);
+        upperInterval = globalUpperInts{metric}(:,alg);
 
         x = [dependentVars, fliplr(dependentVars)];
         y = [lowerInterval; flipud(upperInterval)]';
+        
+        if metric == 2 % Run time
+            y = log(y);
+            %x = log(x);
+        end
+        
         fill(x,y, 1,....
             'facecolor',color, ...
             'edgecolor','none', ...
             'facealpha', 0.3);
 
         hold on
+
+        y = globalMeans{metric}(:,alg);
+        if metric == 2 % Run time
+            y = log(y);
+            %dependentVars = log10(dependentVars);
+        end
         
-        plot(dependentVars,globalMeans{m}(:,alg), 'Color', color);
-        
-        %title(metricLabels{m});
+        plot(dependentVars, y, 'Color', color);
+
 
         xlabel(x_label);
-        ylabel(yLabels{m});
+        ylabel(yLabels{metric});
         
+        y_min = min(y_min, min(y));
+        y_max = max(y_max, max(y));
+        
+    end
 
+          
+    plot_title = strcat([char(96+r), '.) ', testProbLabels{r}]);
+    title(plot_title);
+
+    
+    if metric == 1 % HV 
+        x = dependentVars;
+        y = ones(1, numel(x))*optimal_hvs(r);
+        plot(x,y, '--k', 'LineWidth', 1);
+        y_min = min(y_min, min(y));
+        y_max = max(y_max, max(y));
+        
+    end
+    if metric == 3 % NNDS
+        x = dependentVars;
+        y = ones(1, numel(x))*100;
+        plot(x,y, '--k', 'LineWidth', 1.5);
+        y_min = min(y_min, min(y));
+        y_max = max(y_max, max(y));
+        ylim([y_min - (y_min*0.05), y_max*1.05])
+
+    end
+
+    % Legend logic
+    if (plotno == numAlgorithms || ~grid_layout)
+        
+        % See if you've already added the perfect line legend 
+        if numel(legend_entries) < ((numAlgorithms*2)+1)
+             % Then only add it for HV and NNDS
+            if metric == 1
+               legend_entries{numel(legend_entries)+1} = 'Perfect HV';
+            end
+            if metric == 3
+               legend_entries{numel(legend_entries)+1} = 'Perfect NDS';
+            end
+        end
+
+        legend(legend_entries);
         
     end
     
     
-    legend(legend_entries);
+    plotno = plotno + 1;
+    
     if save_to_file
-        png_file = replace(input_file, '.mat', strcat('_', abbrMetricLabels{m},'.png'));
+        png_file = replace(input_file, '.mat', strcat('_', abbrMetricLabels{metric},'.png'));
         set(gcf,'PaperUnits','inches','PaperSize',[5,5],'PaperPosition',[0 0 6 6]);
         print('-dpng','-r400',png_file);
     end
+
+
     
-end
+end % End for every run 
 
 
 
 %% Functions 
+png_file = replace(input_file, '.mat', strcat('_', abbrMetricLabels{metric},'.png'));
+disp(png_file);
