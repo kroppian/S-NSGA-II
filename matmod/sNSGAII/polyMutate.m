2c2024a37bfb5d808a6fa1abe292bb3947f3234c
Ian Meyer Kropp <kroppian@scully.egr.msu.edu>
2022-03-04 11:17:20 -0500

    Have simple polynomial mutation working (minus out of bound error)

---
+++ matmod/sNSGAII/polyMutate.m
@@ -0,0 +1,33 @@
+function result = polyMutate(genome, lb, ub, eta)
+
+    delta1 = (genome - lb) / (ub - lb);    % Should be between 0 and 1
+    delta2 = (ub - genome) / (ub - lb);    % Should be between 0 and 1
+
+    exp = (eta + 1) ^ -1;
+    
+    ran = rand(size(genome));              % Could be an issue
+    deltaq = zeros(size(genome));
+    
+    leftMask = ran < 0.5;
+    rightMask = ran >= 0.5;
+    
+    xy = 1 - delta1;
+    val = 2.0 * ran + (1.0 - 2.0 * ran) * (xy .^ (eta + 1.0));
+    d = (val .^ exp) - 1.0;
+    deltaq(leftMask) = d(leftMask);
+
+    xy = 1.0 - delta2;
+    val = 2.0 * (1.0 - ran) + 2.0 * (ran - 0.5) * (xy .^ (eta + 1.0));
+    d = 1.0 - (val .^ exp);
+    deltaq(rightMask) = d(rightMask);
+
+    muted_genome = genome + deltaq * (ub - lb);
+    
+    % Todo fix out of bounds errors
+    
+    result = muted_genome;
+
+    
+    
+end
+
---
+++ matmod/sNSGAII/tests/test_polyMutate.m
@@ -0,0 +1,9 @@
+
+foo = ones(500,500)*0.5;
+
+bar = polyMutate(foo, 0, 1, 30);
+
+histogram(bar);
+
+
+
