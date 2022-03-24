065fd5504d9ed358138e2111bf427d7645d14362
Ian Kropp <iankropp@gmail.com>
2022-03-24 10:52:03 -0400

    Pulled out the sparse mutation to target routine into a new func

--- matmod/sNSGAII/sparsePolyMutate.m
+++ matmod/sNSGAII/sparsePolyMutate.m
@@ -52,77 +52,18 @@ function newPop = sparsePolyMutate(Pop, Problem, Parameter)
     lb = zeros(sum(mutateMask), 1);
     ub = ones(sum(mutateMask), 1);
 
-    sparsities2mut = sparsities(mutateMask);
+    newSparsities = sparsities;
 
-    newSparsities = polyMutate(sparsities2mut, lb, ub, distrSMut);
+    newSparsities(mutateMask) = polyMutate(sparsities(mutateMask), lb, ub, distrSMut);
 
     newSparsities = min(max(newSparsities,0),1);
 
-    indv2mut = find(mutateMask);
-
-
-    % determine the non-zero increase/decrease for each indiv
-    nz2add = round(D * (sparsities2mut - newSparsities));
-    
-    % find where the non-zeros are
-    [zIndvs,   zGenes] = find(Pop(indv2mut,:) == 0);
-    % find where the zeros are 
-    [nzIndvs, nzGenes] = find(Pop(indv2mut,:) ~= 0);
-    
-    newNzs = false(size(Pop));
-    newZs = false(size(Pop));
-
-    
-    for i = 1:size(indv2mut,1)
-        % gather relevant info
-        indv_i = indv2mut(i);
-        
-        % Case where more non-zeros are needed
-        if nz2add(i) > 0
-            % find where there are zeros to flip
-            zeroLocs = zGenes(zIndvs == i);
-            
-            % determine how many of them to flip
-            numToFlip = nz2add(i);
-            
-            % Determine which of these posible zero positions to flip
-            toFlip = zeroLocs(randperm(length(zeroLocs), numToFlip));
-
-            % Record these positions 
-            newNzs(indv_i, toFlip) = true;
-            
-        % Case where more zeros are needed
-        else
-            
-            % find where there are non-zeros to flip
-            nZeroLocs = nzGenes(nzIndvs == i);
-            
-            % determine how many of them to flip
-            numToFlip = -nz2add(i);
-
-            % Determine which of these posible non-zero positions to flip
-            toFlip = nZeroLocs(randperm(length(nZeroLocs), numToFlip));
-
-            % Record these positions 
-            newZs(indv_i, toFlip) = true;
-
-        end
-
-
+    % check if there's anything to do
+    if newSparsities == sparsities
+        newPop = Pop;
+    else
+        newPop = sm2target(Pop, newSparsities);
     end
-
-    %% Make the mutations
-    
-    % Find the min/max of the genome positions to mutate 
-    [newNzsRows, ~] = find(newNzs);
-
-    newNzsLb = Problem.lower(newNzsRows);
-    newNzsUb = Problem.upper(newNzsRows);
-
-    Pop(newNzs) = newNzsLb + rand(1,sum(newNzs, 'all')) .* (newNzsUb - newNzsLb);
-    Pop(newZs) = zeros(sum(newZs, 'all'), 1);
-
-    newPop = Pop;
 
 end
 
