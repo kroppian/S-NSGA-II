7de96a885aaccf8fd7f1688e6853548a8e3bacd4
Ian Kropp <iankropp@gmail.com>
2022-04-01 09:53:24 -0400

    Moving the config files into a new folder

--- matmod/main.m
+++ matmod/main.m
@@ -1,10 +1,14 @@
 
 clear
 
-%config_comparative
-%config_effective
-%config_mutationTest
-config_cropoverTest_effective
+addpath("configs");
+
+%comparative
+%effective
+%mutationTest
+%cropoverTest_effective
+cropoverTest_comp
+
 
 %% Run optimization
 res = runOptBatch(config);
@@ -20,8 +24,8 @@ res_final = res(res.gen == res.max_gen,:);
 %% Quick plot
 % Plot the sparsity over generations
 targetSparsity = config.defaultSparsity;
-sample_s_mut_on  = res(res.run == 2 & res.D == 1000 & res.s == targetSparsity & res.s_mut_on, :);
-sample_s_mut_off = res(res.run == 2 & res.D == 1000 & res.s == targetSparsity & (~res.s_mut_on), :);
+sample_s_mut_on  = res(res.run == 1 & res.D == 1000 & res.s == targetSparsity & res.s_mut_on, :);
+sample_s_mut_off = res(res.run == 1 & res.D == 1000 & res.s == targetSparsity & (~res.s_mut_on), :);
 
 subplot(2,1,1);
 plot(sample_s_mut_on.medSparsities); hold on;
@@ -39,6 +43,19 @@ plot(sample_s_mut_off.HV);
 legend("sparse mutation on", "sparse mutation off");
 xlabel("Generation");
 ylabel("Median solution HV");
+
+%% Checking out individual fronts
+run = 3;
+s_mut_on_pop = res_final{res_final.run == run & res_final.D == 100 & res_final.alg == "sNSGAII", 'population'};
+s_mut_off_pop = res_final{res_final.run == run & res_final.D == 100 & res_final.alg == "SparseEA2", 'population'};
+
+s_mut_on_pop = s_mut_on_pop.best.objs;
+s_mut_off_pop = s_mut_off_pop.best.objs; 
+
+figure
+scatter(s_mut_on_pop(:,1), s_mut_on_pop(:,2));
+hold on;
+scatter(s_mut_off_pop(:,1), s_mut_off_pop(:,2));
 
 
 %% Full metric plots
