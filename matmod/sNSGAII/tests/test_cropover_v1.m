77c772bf7a323c55a493b1cfe159337e54fdc081
Ian Kropp <iankropp@gmail.com>
2022-04-25 13:22:41 -0400

    Fixing a very annoying bug with SMOP creation

--- matmod/sNSGAII/tests/test_cropover_v1.m
+++ matmod/sNSGAII/tests/test_cropover_v1.m
@@ -6,12 +6,11 @@ addpath("../..");
 addpath("../../configs");
 addpath("../../utilities");
 
-sNSGA_eff_decVar;
+%sNSGA_eff_decVar;
+sNSGA_eff_400
 
 D = 100;
-prob = SMOP1('N', 2, 'D', D, 'parameter', {0.15});
-
-% prob = SMOP1(0.4);
+prob = SMOP1('N', 100, 'D', D, 'FE', 200, 'maxFE', 20000, 'parameter', {0.15});
 
 pop = sparseSampler(prob, 0.75, 0.75);
 
@@ -25,12 +24,16 @@ resulting_sparsities = ones(reps,2)*-99;
 
 for r = 1:reps
 
-    new_pop = cropover_v1(pop.decs, prob.lower, prob.upper);
+    new_pop = cropover_v2(pop.decs, prob.lower, prob.upper);
 
     sparsities = sum(new_pop == 0,2)/D;
     resulting_sparsities(r,1) = sparsities(1);
     resulting_sparsities(r,2) = sparsities(2);
-  
+    
+    if mod(r,100) == 0
+        fprintf("%d\n", r);
+    end
+    
 end
 
 histogram(resulting_sparsities,20);
