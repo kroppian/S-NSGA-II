9ce7e8289b9d8a373f4a884ced0ae6ff395eea5b
Ian Kropp <iankropp@gmail.com>
2022-05-05 15:36:29 -0400

    Second swing at this algorithm

--- matmod/sNSGAII/VariedStripedSparseSampler_v1.m
+++ matmod/sNSGAII/VariedStripedSparseSampler_v1.m
@@ -1,109 +1,122 @@
 %% Randomly generate an initial population
-function Population = VariedStripedSparseSampler(prob, sLower, sUpper)
+function Population = VariedStripedSparseSampler_v1(prob, sLower, sUpper)
     
+
+    %  Nomenclature example
+    %  N = 8 
+    %  D = 14
+    %  
+    %          Cycle length of 14 
+    %          |
+    %  |-------|-----------------|
+    %  1 1 1 1                        -
+    %          1 1 1 1                |-- One full cycle 
+    %                  1 1 1          |
+    %                        1 1 1    -
+    %     |-------|------|-----|---------------- Cycle count of 4
+    %   |---|--|-|----------------------Cycle count of 4 
+    %  1 1 
+    %      1 1 
+    %          1 
+    %            1   
+    %  |----|----|
+    %       | 
+    %       Cyle length of 6
+
     pop = prob.Initialization();
     varCount = size(prob.lower,2);
     mask = false(prob.N, varCount);
 
     densityVector = 1 - linspace(sLower, sUpper, prob.N);
+    widthVector = round(densityVector.*prob.D);
+    cumulativeWidths = cumsum(widthVector);
 
-    
-    stripePosition = 1; 
-    leftToRight = true;
-
-    for i = 1:prob.N
-        stripeWidth = round(densityVector(i)*prob.D);
-
-        if leftToRight
-            mask(i, stripePosition:(stripePosition+stripeWidth-1)) = true; 
-        else            
-            mask(i, (stripePosition-stripeWidth):stripePosition) = true; 
-        end
-
-        % Move to the next position (either leftward or rightward)
-        if leftToRight
-            stripePosition = stripePosition + stripeWidth;
-        else
-            stripePosition = stripePosition - stripeWidth - 1;
-        end
-
-        if i ~= prob.N 
-
-            nextStripeWidth = round(densityVector(i+1)*prob.D);
-
-            % check if the next stripe will fit
-            if leftToRight && stripePosition + nextStripeWidth - 1 > prob.D
-                stripePosition = prob.D;
-                leftToRight = false;
-            elseif (~leftToRight) && stripePosition - nextStripeWidth < 1
-                stripePosition = 1;
-                leftToRight = true;
-            end
-
-        end
+    processedIndvs = 0;
+    cycle_count = 1;
+    cycles = zeros(prob.N, prob.D);
+    while processedIndvs < prob.N
+        % Figure out how many stripes will fit in this cycle 
+        spotsThatFitMask = cumulativeWidths <= prob.D & cumulativeWidths ~= 0;
+        numThatFit = sum(spotsThatFitMask);
+        largestFit = max(cumulativeWidths(spotsThatFitMask));
+        cumulativeWidths = cumulativeWidths - largestFit; 
+        cumulativeWidths(cumulativeWidths<0) = 0; 
+        processedIndvs = processedIndvs + numThatFit;
+        spotsThatFit = find(spotsThatFitMask);
+        cycles(cycle_count,1:numThatFit) = spotsThatFit;   
+        cycle_count = cycle_count + 1;
 
     end
 
-    sparse_pop = pop.decs;
-    sparse_pop(mask) = 0;
+    gapPosition = 1; 
+    currentIndv = 1;
+    for c = 1:cycle_count
+        cycle = cycles(c, cycles(c, :) ~= 0);
 
-    Population = SOLUTION(sparse_pop);
+        widths = widthVector(cycle);
+        if(gapPosition > numel(widths))
+            gapPosition = 1;
+        end
 
-%     %% Build stripes
-%     avgSparsity = mean([sLower, sUpper]);
-%     
-%     stripeWidths = calcStripeWidths(avgSparsity, prob.D);
+        gapSize = prob.D - sum(widths);
+
+        position = 1;
+        for i = 1:numel(widths)
+
+            width = widths(i);
+
+            if gapSize ~= 0 && gapPosition == i
+                position = position+gapSize;
+            end 
+            mask(currentIndv, position:position+width-1) = true; 
+
+
+            % Go to the next individual 
+            position = position+width;
+            currentIndv = currentIndv + 1;
+        end
+
+        if gapSize ~= 0
+            gapPosition = gapPosition + 1;
+        end
+    end
+
+    foo = zeros(size(mask));
+    foo(mask) = 1;
+    heatmap(foo);
+
+    a = 1;
+
+%     shiftVector = (1:prob.N)';
 % 
-%     stripeCount = numel(stripeWidths);
 % 
-%     cycles = prob.N/stripeCount;
+%     foo = repmat(densityVector, prob.N);
 % 
-%     stripePos = 1; 
-%     for sw = 1:stripeCount*cycles
-%         mask(sw, :) = true;
-%         currentStripeWidth = mod((sw-1), stripeCount) + 1;
+%     current_indv = 1; 
 % 
-%         mask(sw, stripePos:(stripePos + stripeWidths(currentStripeWidth) - 1)) = false;
-%         
-%         stripePos = (stripePos + stripeWidths(currentStripeWidth));
-%     
-%         if stripePos > prob.D
-%             stripePos = 1;
+%     % Go through and build each cycle 
+%     while current_indv <= prob.N
+% 
+% 
+%         cycle_count = 1; 
+%         cycle_length = 0; 
+% 
+%         buffer = [];
+% 
+%         % Through through each population member of the current cycle 
+%         while cycle_length < prob.D || current_indv <= prob.N
+%             
+%             buffer()
+% 
+%             cycle_count = cycle_count + 1; 
+%             current_indv = current_indv + 1; 
 %         end
-%     
-%     end
-%    
+%         
 % 
-% 
-%     %% Sparse sample for the rest of the population 
-%     for indv = (stripeCount*cycles+1):prob.N
-%         % generate the number of zeros 
-%         zeroCount = randi(round([sLower*varCount, sUpper*varCount]));
-%         zeroIndices = randperm(varCount, zeroCount);
-%         mask(indv, zeroIndices) = true;
 %     end
-%     sparse_pop = pop.decs;
-%     
-%     sparse_pop(mask) = 0;
-%     
-%     Population = SOLUTION(sparse_pop);
-% 
-%     function stripeWidths = calcStripeWidths(s, D)
-%         stripeWidth = floor(round((1-s)*D, 5));
-%     
-%         stripeCount = floor(D/stripeWidth);
-%     
-%         stripeRemainder = D - stripeWidth*stripeCount;
-%     
-%         stripeWidths = ones(stripeCount,1)*stripeWidth;
-%     
-%         stripeWidths = stripeWidths + floor(stripeRemainder/stripeCount);
-%     
-%         finalRemainder =  D - sum(stripeWidths);
-%     
-%         stripeWidths(end) = stripeWidths(end) + finalRemainder;
-%    
-%     end
+%      
+
+
 
 end
 
