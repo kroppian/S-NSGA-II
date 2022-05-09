7d10cb3d39934553910ad324923fcb6bc808d527
Ian Kropp <iankropp@gmail.com>
2022-05-09 10:47:30 -0400

    Finally have really good spread

--- matmod/sNSGAII/VariedStripedSparseSampler_v2.m
+++ matmod/sNSGAII/VariedStripedSparseSampler_v2.m
@@ -34,10 +34,11 @@ function Population = VariedStripedSparseSampler_v2(prob, sLower, sUpper)
     cumulativeWidths = cumsum(widthVector);
 
     processedIndvs = 0;
-    cycle_count = 1;
+    cycle_count = 0;
     cycles = zeros(prob.N, prob.D);
     while processedIndvs < prob.N
         % Figure out how many stripes will fit in this cycle 
+        cycle_count = cycle_count + 1;
         spotsThatFitMask = cumulativeWidths <= prob.D & cumulativeWidths ~= 0;
         numThatFit = sum(spotsThatFitMask);
         largestFit = max(cumulativeWidths(spotsThatFitMask));
@@ -46,33 +47,56 @@ function Population = VariedStripedSparseSampler_v2(prob, sLower, sUpper)
         processedIndvs = processedIndvs + numThatFit;
         spotsThatFit = find(spotsThatFitMask);
         cycles(cycle_count,1:numThatFit) = spotsThatFit;   
-        cycle_count = cycle_count + 1;
 
     end
 
     %% Create density mask 
+
+    % Mask out non-zero values cycle-by-cycle
     currentIndv = 1;
     for c = 1:cycle_count
         cycle = cycles(c, cycles(c, :) ~= 0);
 
+        final_cycle = c == cycle_count;
+
         widths = widthVector(cycle);
 
         gapToFill = prob.D - sum(widths);
-        
+        gapSize = ceil((prob.D - sum(widths))/numel(widths));
+        % For each individual in the cycle 
         position = 1;
         for i = 1:numel(widths)
 
             width = widths(i);
 
+            % Determine if a gap is needed 
             gapWidth = 0;
             if gapToFill > 0
-                gapWidth = 1;
-                gapToFill = gapToFill - 1;
+                gapWidth = gapSize;
+                gapToFill = gapToFill - gapWidth;
             end
 
-            mask(currentIndv, position:position+width+gapWidth-1) = true; 
+            % Determine the position of the stripe
+            startPoint = position;
+            if final_cycle 
+                % if the final cyle, fill gap with zero padding
+                endPoint = position+width-1; 
+            else
+                % if not the final cycle, fill gap with non-zero padding
+                endPoint = position+width+gapWidth-1;
+            end
+            
+
+            % Prevent overflow from a gap calculation
+            if endPoint > prob.D 
+                endPoint = prob.D;
+            end
+    
+            % Mask out stripe
+            mask(currentIndv, startPoint:endPoint) = true; 
 
             % Go to the next individual 
+            
             position = position+width+gapWidth;
             currentIndv = currentIndv + 1;
 
