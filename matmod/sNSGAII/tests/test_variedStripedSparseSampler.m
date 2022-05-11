a400e59e1703090fa41b1b51ba1a160e68210600
Ian Kropp <kroppian@msu.edu>
2022-05-11 09:41:11 -0400

    Completing the varied striped sampler test script

--- matmod/sNSGAII/tests/test_variedStripedSparseSampler.m
+++ matmod/sNSGAII/tests/test_variedStripedSparseSampler.m
@@ -13,7 +13,16 @@ prob = SMOP1('N', 100, 'D', D, 'FE', 200, 'maxFE', 20000, 'parameter', {0.15});
 % No clue why this is needed, but taken from ALGORITHM.Solve
 prob.Current(prob);
 
-pop = VariedStripedSparseSampler(prob, 0.50, 1);
+pop = VariedStripedSparseSampler_v2(prob, 0.75, 1);
 
 pop_genes = pop.decs;
+
+nz_mask = pop_genes ~= 0;
+
+nz_map = zeros(size(nz_mask));
+nz_map(nz_mask) = 1; 
+
+heatmap(nz_map);
+figure;
+bar(sum(nz_map));
 
