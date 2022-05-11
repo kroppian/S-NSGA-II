220399882c5be8c062d348a5450b45f925387609
Ian Kropp <kroppian@msu.edu>
2022-05-11 10:05:35 -0400

    Adding a batch version of main to run all problems in one go

---
+++ matmod/main_batch.m
@@ -0,0 +1,51 @@
+clear
+
+addpath("configs");
+addpath("utilities");
+addpath("plotting");
+
+problems = {@SMOP1, @SMOP2, SMOP3, SMOP4, SMOP5, SMOP6, SMOP7, SMOP8};
+
+%sNSGA_eff_400;
+%sNSGA_eff_sparsity;
+%sNSGA_comparative;
+sNSGA_eff_decVar;
+
+
+for p = 1:numel(problems)
+
+    config.prob = problems{p};
+
+    if config.saveData
+        file_name = strcat(config.run_label, '_', config.runType, '_', strrep(char(config.prob),'@(x)',''), '.mat');
+        fullSavePath = strcat(config.savePath, file_name);
+        if ~exist(config.savePath, 'dir')
+            error("Output path does not exist.");
+        end
+    end
+    
+    
+    %% Run optimization
+    res = runOptBatch(config);
+    
+    
+    %% Post processing
+    medSparsities = calcMedianSparsities(res);
+    res.medSparsities = medSparsities;
+    
+    % Get a last generation cross section of the results 
+    res_final = res(res.gen == res.max_gen,:);
+    
+    if config.saveData
+        if config.saveAllGens
+            save(fullSavePath, 'res_final', 'res', 'config', '-v7.3');
+        else        
+            save(fullSavePath, 'res_final', 'config', '-v7.3');
+        end
+    end
+
+end
+
+
+
+
