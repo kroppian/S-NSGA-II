621581fee7f26ca79fdc2588617e3807870f62f1
Ian Kropp <iankropp@gmail.com>
2022-05-12 12:45:45 -0400

    Incorporating the varied sparse sampler

--- matmod/sNSGAII/tests/test_cropover.m
+++ matmod/sNSGAII/tests/test_cropover.m
@@ -14,11 +14,19 @@ prob = SMOP1('N', 100, 'D', D, 'FE', 200,'maxFE', 20000, 'parameter', {0.15});
 % No clue why this is needed, but taken from ALGORITHM.Solve
 prob.Current(prob);
 
-pop = sparseSampler(prob, 0.75, 0.75);
+sub = 1; 
+slb = 0.5; 
 
-sparsities = sum(pop.decs == 0,2)/D;
-sparsity1 = sparsities(1);
-sparsity2 = sparsities(2);
+pop = VariedStripedSparseSampler_v2(prob, slb, sub);
+
+original_sparsities = sum(pop.decs == 0,2)/D;
+
+subplot(2,1,1);
+histogram(original_sparsities,0.5/0.001);
+
+
+title(sprintf("%0.2f to %0.2f", slb, sub));
+
 
 reps = 1000;
 
@@ -28,14 +36,28 @@ for r = 1:reps
 
     new_pop = cropover_v2(pop.decs, prob.lower, prob.upper);
 
-    sparsities = sum(new_pop == 0,2)/D;
-    resulting_sparsities(r,1) = sparsities(1);
-    resulting_sparsities(r,2) = sparsities(2);
+    original_sparsities = sum(new_pop == 0,2)/D;
+    resulting_sparsities(r,1) = original_sparsities(1);
+    resulting_sparsities(r,2) = original_sparsities(2);
     
 end
 
 resulting_sparsities = reshape(resulting_sparsities, [], 1);
 
-histogram(resulting_sparsities,20);
+subplot(2,1,2);
 
-std(resulting_sparsities)
+histogram(resulting_sparsities,0.5/0.001);
+
+stddev = std(resulting_sparsities);
+
+lb = mean(resulting_sparsities) - stddev*2;
+ub = mean(resulting_sparsities) + stddev*2;
+fprintf("Lower bound: %f\n", lb);
+fprintf("Upper bound: %f\n", ub);
+fprintf("Spread: %f\n", ub - lb);
+fprintf("Island count: %f\n", 0.5/(ub - lb));
+
+
+
+
+
