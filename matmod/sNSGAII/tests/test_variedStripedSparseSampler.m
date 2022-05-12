9f3fd62889619184f7651ad185e57d6bb69cb08a
Ian Kropp <iankropp@gmail.com>
2022-05-12 13:30:48 -0400

    Adding a sparsity measure

--- matmod/sNSGAII/tests/test_variedStripedSparseSampler.m
+++ matmod/sNSGAII/tests/test_variedStripedSparseSampler.m
@@ -14,7 +14,7 @@ prob = SMOP1('N', 100, 'D', D, 'FE', 200, 'maxFE', 20000, 'parameter', {0.15});
 prob.Current(prob);
 
 pop = VariedStripedSparseSampler_v2(prob, 0.75, 1);
-
+`
 pop_genes = pop.decs;
 
 nz_mask = pop_genes ~= 0;
@@ -25,4 +25,9 @@ nz_map(nz_mask) = 1;
 heatmap(nz_map);
 figure;
 bar(sum(nz_map));
+
+
+sparsities = sum(pop_genes == 0,2)/D;
+figure;
+histogram(sparsities);
 
