f458b1fe7999b0184445ef7ff47d07857478b238
Ian Kropp <iankropp@gmail.com>
2022-05-31 14:19:29 -0400

    Tried linear programming without much luck

--- matmod/sNSGAII/tests/test_variedStripedSparseSampler.m
+++ matmod/sNSGAII/tests/test_variedStripedSparseSampler.m
@@ -8,13 +8,13 @@ addpath("../../utilities");
 
 sNSGA_eff_400;
 
-D = 50;
+D = 100;
 prob = SMOP1('N', 100, 'D', D, 'FE', 200, 'maxFE', 20000, 'parameter', {0.15});
 % No clue why this is needed, but taken from ALGORITHM.Solve
 prob.Current(prob);
 
-pop = VariedStripedSparseSampler_v2(prob, 0.75, 1);
-`
+pop = SamplerNouveau(prob, 0.75, 1);
+
 pop_genes = pop.decs;
 
 nz_mask = pop_genes ~= 0;
@@ -29,5 +29,5 @@ bar(sum(nz_map));
 
 sparsities = sum(pop_genes == 0,2)/D;
 figure;
-histogram(sparsities);
+histogram(sparsities, 100);
 
