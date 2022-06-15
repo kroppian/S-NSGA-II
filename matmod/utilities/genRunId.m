c517b649c772cdbca028fc46ff4f0b103e74147b
Ian Kropp <kroppian@msu.edu>
2022-06-15 12:32:15 -0400

    Merging the functionality of these two functions

--- matmod/utilities/genRunId.m
+++ matmod/utilities/genRunId.m
@@ -1,11 +1,16 @@
-function id = genRunId(config,runNo)
+function id = genRunId(config, runNo)
 
-    alg_name = func2str(config.algorithms{runNo});
-    sampling_method = func2str(config.sampling_method{runNo}{1});
-    mutation_method = func2str(config.mutation_method{runNo});
-    crossover_method = func2str(config.crossover_method{runNo});
+    raw_algorithm = func2str(config.algorithms{runNo});
 
-    id = [alg_name, '-', sampling_method, '-', mutation_method, '-', crossover_method];
+    if raw_algorithm == "sNSGAII" || raw_algorithm == "sNSGAII_island_v1" || raw_algorithm == "sNSGAII_island_v2"
+        sampling_method =  func2str(config.sampling_method{runNo}{1});
+        mutation_method = func2str(config.mutation_method{runNo});
+        crossover_method = func2str(config.crossover_method{runNo});
+
+        id = [raw_algorithm, '-', sampling_method, '-', mutation_method, '-', crossover_method];
+    else
+        id = raw_algorithm;
+    end
 
 end
 
