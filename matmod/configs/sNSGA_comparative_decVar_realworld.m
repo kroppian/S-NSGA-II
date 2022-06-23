476e68ad3fde570b4d702a369391b4d3ec74af14
Ian Kropp <iankropp@gmail.com>
2022-06-23 13:44:49 -0400

    Run for Sparse_NN

--- matmod/configs/sNSGA_comparative_decVar_realworld.m
+++ matmod/configs/sNSGA_comparative_decVar_realworld.m
@@ -1,7 +1,7 @@
 
 max_ref = 7; 
 
-comp = "WORKPC";
+comp = "GILG";
 
 if comp == "DECS"
     platPath = 'M:\Projects\PlatEMO_3.4.0\PlatEMO\';
@@ -19,17 +19,16 @@ end
 
 
 
-algorithms =   {@SparseEA, @SparseEA2, @MOEAPSL, @MPMMEA, @PMMOEA,  @sNSGAII         };
-mutation_op =  {@nop     , @nop      , @nop    , @nop   , @nop   ,  @sparsePolyMutate};
-crossover =    {@nop     , @nop      , @nop    , @nop   , @nop   ,  @cropover_v2     };
+algorithms =   {@SparseEA, @SparseEA2, @MOEAPSL, @PMMOEA,  @sNSGAII         };
+mutation_op =  {@nop     , @nop      , @nop    , @nop   ,  @sparsePolyMutate};
+crossover =    {@nop     , @nop      , @nop    , @nop   ,  @cropover_v2     };
 
-pop_samplers = {@nop, @nop, @nop, @nop, @nop, ...
+pop_samplers = {@nop, @nop, @nop, @nop, ...
                  {@VariedStripedSparseSampler_v3, 0.75, 1}};
 
 labels = ["SparseEA", ...
           "SparseEA2", ...
           "MOEA-PSL", ...
-          "MP-MMEA", ...
           "PM-MOEA", ...
           "sNSGA-II with VSSPS" ];
 
@@ -49,11 +48,11 @@ config = run_config(platPath,                                                 ..
                     true,                                                     ...    %   indep_var_dec_vars
                     100,                                                      ...    %   defaultDecVar     
                     0.1,                                                      ...    %   defaultSparsity   
-                    [40],                                                     ...    %   (Really size of hidden layer)                          
+                    [20, 40, 80, 160, 320],                                   ...    %   (Really size of hidden layer)                          
                     linspace(0.05, 0.45,2),                                   ...    %   sparsities        (TODO revert)
                     "compDecVar",                                             ...    %   runType           
                     true,                                                     ...    %   saveData
-                    "C:\Users\Ian Kropp\Desktop",                             ...    %   savePath
+                    "/mnt/nas/kroppian/sNSGAIIRuns",                          ...    %   savePath
                     false,                                                    ...    %   saveAllGens
                     1                                                         ...    %   subproblem 
                     ); 
