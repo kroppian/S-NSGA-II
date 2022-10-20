3a28842a6641012c86f28c1737b49d1b0ea095e7
Ian Kropp <iankropp@gmail.com>
2022-10-19 20:42:04 -0400

    Adding hacky update to getAlgID

--- matmod/utilities/getAlgId.m
+++ matmod/utilities/getAlgId.m
@@ -3,7 +3,12 @@ function id = getAlgId(config, runNo)
     raw_algorithm = func2str(config.algorithms{runNo});
 
     if raw_algorithm == "sNSGAII" || raw_algorithm == "sNSGAII_island_v1" || raw_algorithm == "sNSGAII_island_v2"
-        sampling_method =  func2str(config.sampling_method{runNo}{1});
+        
+        if class(config.sampling_method{runNo}) == "function_handle" && func2str(config.sampling_method{runNo}) == "nop"
+            sampling_method = "uniform";
+        else
+            sampling_method = func2str(config.sampling_method{runNo}{1});
+        end
         mutation_method = func2str(config.mutation_method{runNo});
         crossover_method = func2str(config.crossover_method{runNo});
 
